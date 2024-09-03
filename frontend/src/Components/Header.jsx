06ae733856ce246d87b441eb44bab3e601da9884
Quiks <ffasoli9@gmail.com>
2024-09-03 15:13:06 -0300

    Login & header fully responsive

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import "./Css/Header.css";
-import { FaUser } from "react-icons/fa";
 import { IoMenu  } from "react-icons/io5";
+import LoginIcon from '@mui/icons-material/Login';
 import { Link } from 'react-router-dom';
 import logoNDsolo from './assets/logoNDsolo.png'
+import Logo from './assets/logo.png'
 
 const header = ({showMenu, setShowMenu}) => {
   return (
@@ -12,13 +13,14 @@ const header = ({showMenu, setShowMenu}) => {
       {showMenu}
       <div className="btnMenu" onClick={() => setShowMenu(!showMenu)}><IoMenu/></div>
       <div id="headerT">
-        <img src={logoNDsolo} alt="logo" />
+        <img src={logoNDsolo} alt="logo" id='logo' />
+        <img src={Logo} alt="logo" id='logoM' />
         <div className="headerText">
           Nova-Domus
         </div>
       </div>
       <div className="loginHeader">
-        <Link to= "/login"><FaUser/> Inicia Sesion</Link>
+        <Link to= "/login" id='loginIcon'><LoginIcon/></Link> <Link to= "/login" id='loginT'> Inicia Sesion</Link>
       </div>
     </div>
   )
